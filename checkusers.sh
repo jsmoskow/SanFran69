b94acce724f4e2751e81c636f21d66a8e6fa84ea
Epixepiste <111820426+Epixepiste@users.noreply.github.com>
2024-02-29 13:19:25 -0500

    Update checkusers.sh

--- checkusers.sh
+++ checkusers.sh
@@ -3,7 +3,7 @@
 expectedusers=("root" "daemon" "bin" "sys" "sync" "games" "man" "lp" "mail" "news" "uucp" "proxy" "www-data" "backup" "list" "irc" "nobody" "systemd-network"  "dhcpcd" "systemd-resolve" "systemd-timesync" "messagebus" "syslog" "_apt" "system-oom" "avahi-autoipd" "kernoops" "whoopsie" "dnsmasq" "avahi" "cups-pk-helper" "speech-dispatcher" "fwupd-refresh" "saned" "geoclue" "cups-broswed" "hplip" "polkitd" "rtkit" "colord" "gnome-initial-setup" "gdm" "nm-openvpn" "tss" "uuidd" "tcpdump" "landscape" "pollinate" "sshd" "ubuntu" "BanditAlex" "MarshalJustice" "PrairiePioneer" "OutlawOutlook" "CalamityJane" "SheriffMcCoy" "SaloonSally" "DocHolliday")
 
 for user in $(cut -f1 -d: /etc/passwd); do
- if [[ ! "${expectedusers[@]} " =~ $user " ]]; then
+ if [[ ! "${expectedusers[@]}" =~ $user ]]; then
   echo "$user"
  fi
 done
