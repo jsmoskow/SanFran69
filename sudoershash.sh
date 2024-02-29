2d2ca9734a456639db6132128d3a13280dfabdb8
Epixepiste <111820426+Epixepiste@users.noreply.github.com>
2024-02-29 13:18:17 -0500

    Update sudoershash.sh

--- sudoershash.sh
+++ sudoershash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-expectedhash="5bac27ce5ff1a78ace8f3ef3ef81bfd60cbd44810ac3f3d280da9d7649fe90c18f8"
+expectedhash=5bac27ce5ff1a78ace8f3ef3ef81bfd60cbd44810ac3f3d280da9d7649fe90c18f8
 
 sudoershash=$(sha256sum /etc/sudoers | awk '{print $1}')
 
