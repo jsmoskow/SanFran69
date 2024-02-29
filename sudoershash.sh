486ec90ee7f0406d233aaa78018a0ffb03b179b5
Epixepiste <111820426+Epixepiste@users.noreply.github.com>
2024-02-29 13:30:06 -0500

    Update sudoershash.sh

--- sudoershash.sh
+++ sudoershash.sh
@@ -3,8 +3,6 @@
 expectedhash=8c20cd717552790f2312db0981337945
 
 sudoershash=$(md5sum /etc/sudoers | awk '{print $1}')
-echo $expectedhash
-echo $sudoershash
 
 if [ "$sudoershash" = "$expectedhash" ]; then
     echo "sudoers is unchanged from default"
