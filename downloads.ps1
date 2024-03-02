50069af03e7c034dfee423270f452ea252c7524f
Ben-Juliano619 <111253855+Ben-Juliano619@users.noreply.github.com>
2024-03-01 21:59:26 -0500

    Add files via upload

---
+++ downloads.ps1
@@ -0,0 +1,8 @@
+Invoke-WebRequest -Uri "https://download.sysinternals.com/files/Autoruns.zip" -OutFile "$env:USERPROFILE\Documents\ar.zip"
+Invoke-WebRequest -Uri "https://download.sysinternals.com/files/TCPView.zip" -OutFile "$env:USERPROFILE\Documents\tv.zip"
+Invoke-WebRequest -Uri "https://download.sysinternals.com/files/Sysmon.zip" -OutFile "$env:USERPROFILE\Documents\sm.zip "
+Invoke-WebRequest -Uri "https://download.sysinternals.com/files/ProcessExplorer.zip" -OutFile "$env:USERPROFILE\Documents\pe.zip"
+Invoke-WebRequest -Uri "https://download.sysinternals.com/files/ProcessMonitor.zip" -OutFile "$env:USERPROFILE\Documents\pm.zip"
+Invoke-WebRequest -Uri "https://1.na.dl.wireshark.org/win64/Wireshark-win64-latest.exe" -OutFile "$env:USERPROFILE\Documents\ws.exe"
+
+
