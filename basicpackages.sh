98e5f3f33b567731b0356c1092c2d1cea03d6503
Vasu-Baldwa <35380065+Vasu-Baldwa@users.noreply.github.com>
2024-03-01 21:19:02 -0500

    Updated package.

--- basicpackages.sh
+++ basicpackages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update 
-sudo apt install -y wireshark rkhunter lynis busybox htop inotify-tools
+sudo apt install -y wireshark rkhunter lynis busybox htop inotify-tools micro 
 
 
