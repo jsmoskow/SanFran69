0201e8964bee4147423b7bba6d73ac2fbb189855
Epixepiste <111820426+Epixepiste@users.noreply.github.com>
2024-03-01 21:49:26 -0500

    Update listnetworkconnection.sh

--- listnetworkconnection.sh
+++ listnetworkconnection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for connection in $(lsof -i | awk '); do
+lsof -i | awk '
     $1 == "COMMAND" || $1 == "tcp" || $1 == "udp" {
         if ($1 == "COMMAND") {
             printf "%-20s %-10s %-25s %s\n", "Process Name", "PID", "Connection Address", "User"
