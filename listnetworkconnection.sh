e51082f7f50203f3bbba685b0dd4f4383ff5e3e6
Epixepiste <111820426+Epixepiste@users.noreply.github.com>
2024-03-01 21:46:16 -0500

    Update and rename listnetworkconnection to listnetworkconnection.sh

--- listnetworkconnection.sh
+++ listnetworkconnection.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-lsof -i | awk '
+for connection in $(lsof -i | awk '); do
     $1 == "COMMAND" || $1 == "tcp" || $1 == "udp" {
         if ($1 == "COMMAND") {
             printf "%-20s %-10s %-25s %s\n", "Process Name", "PID", "Connection Address", "User"
